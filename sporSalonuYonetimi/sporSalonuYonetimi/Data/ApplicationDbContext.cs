4410b900ee0b16423d9688ee4ecfe815f412d426
ahmetcan54 <124576317+ahmetcan54@users.noreply.github.com>
2025-12-12 00:14:44 +0300

    web-programlama-proje

---
+++ sporSalonuYonetimi/sporSalonuYonetimi/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sporSalonuYonetimi.Data
+{
+    public class ApplicationDbContext : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
